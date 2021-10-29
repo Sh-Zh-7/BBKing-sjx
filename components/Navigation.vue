822f627708a979d5b630df6cc4b7a13161f5b570
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-10-29 23:05:33 +0800

    style(comp): adjust navigation bar's padding and text size.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,17 +1,17 @@
 <template>
-  <header class='w-full h-12 pt-3 bg-green-500'>
+  <header class='w-full h-16 pt-5 bg-green-500'>
     <nav class='text-center'>
       <NuxtLink to='/'>
-        <span class='px-5 text-lg font-medium text-white'>首页</span>
+        <span class='px-10 text-xl font-medium text-white'>首页</span>
       </NuxtLink>
       <NuxtLink to='/sayings'>
-        <span class='px-5 text-lg font-medium text-white'>醒世恒言</span>
+        <span class='px-10 text-xl font-medium text-white'>醒世恒言</span>
       </NuxtLink>
       <NuxtLink to='/videos'>
-        <span class='px-5 text-lg font-medium text-white'>父亲们の爱</span>
+        <span class='px-10 text-xl font-medium text-white'>父亲们の爱</span>
       </NuxtLink>
       <NuxtLink to='/about'>
-        <span class='px-5 text-lg font-medium text-white'>其他</span>
+        <span class='px-10 text-xl font-medium text-white'>其他</span>
       </NuxtLink>
     </nav>
   </header>
