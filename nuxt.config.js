b76c7e68c04e464c350a62cb355a47d2c52b34c9
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-10-29 12:06:52 +0800

    chore(title): change title to zh-CN.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'BBKing-sjx',
+    title: 'BBの神：SJX',
     htmlAttrs: {
       lang: 'en'
     },
