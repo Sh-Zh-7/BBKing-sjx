30956984be36b742463f0d54dd20a38d2abc6e1a
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-10-29 15:01:48 +0800

    feat(index): add before and after pseudo class for the title.

--- pages/index.vue
+++ pages/index.vue
@@ -23,29 +23,46 @@
       </section>
 
       <section>
-        <h1 class='text-3xl text-center'>经典永流传</h1>
+        <p id="random-quote" class='text-3xl text-center mt-7'>经典永流传</p>
         <div class='flex items-center justify-center'>
           <img src='/avatar.png' alt='avatar of sjx'
-               class='rounded-full w-36 h-36 border-green-600 border-4 p-1.5'>
+               class='rounded-full w-36 h-36 border-green-400 border-4 p-1.5'>
           <div class='ml-14'>
-            <div class='w-5 h-5 bg-black triangle inline-block'></div>
-            <div class='bg-black inline-block -ml-4 p-5'>
-              <p class='text-white'>月薪过万不是刚刚站在社会底层？</p>
+            <div class='w-5 h-5 triangle inline-block border-gray-600 border-2'></div>
+            <div class='inline-block -ml-4 p-5 border-gray-600 border-2 rounded-2xl shadow-xl'>
+              <p>月薪过万不是刚刚站在社会底层？</p>
             </div>
           </div>
         </div>
       </section>
     </main>
-    <footer class='w-full mt-10 bg-gray-800'>
-      <div class="text-center text-white py-3">Copyright © 2021 ShZh7 </div>
+    <footer class='w-full mt-10 bg-green-500'>
+      <div class="text-center text-white py-3">Copyright © 2021 ShZh7</div>
     </footer>
   </div>
 </template>
 
 <style>
+#random-quote::before, #random-quote::after {
+  content: "";
+  font-size: 0;
+  border-width: 7px 150px;
+}
+
+#random-quote::before {
+  border-color: transparent rgb(52, 211, 153) rgb(52, 211, 153) transparent;
+}
+#random-quote::after {
+  border-color: transparent transparent rgb(52, 211, 153) rgb(52, 211, 153);
+}
+
 .triangle {
   clip-path: polygon(0% 0%, 100% 100%, 0% 100%);
   transform: rotate(45deg);
+  margin-right: -0.2px;
+
+  background-color: white;
+  z-index: 1;
 }
 </style>
 
