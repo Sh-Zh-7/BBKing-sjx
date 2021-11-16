eace62c36ed0374efd5bf0b7f31f8905726c3e73
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-11-16 10:38:42 +0800

    chore: move payment info to cloudinary.

--- pages/about.vue
+++ pages/about.vue
@@ -3,8 +3,8 @@
     <Title title='打钱！' />
     <p class='text-center mb-8 px-10 text-center'>别的话我就不多说了嗷，喜欢这个网站就请作者喝☕️</p>
     <div class='flex flex-col items-center md:justify-center md:flex-row'>
-      <img src='../static/alipay.jpeg' alt='支付宝付款' class='h-96 mb-10 md:mb-0 md:mr-20'>
-      <img src='../static/wechat_pay.png' alt='微信付款' class='h-96'>
+      <img src='https://res.cloudinary.com/shzh7/image/upload/v1637030166/payment/alipay_x0lesm.jpg' alt='支付宝付款' class='h-96 mb-10 md:mb-0 md:mr-20'>
+      <img src='https://res.cloudinary.com/shzh7/image/upload/v1637030172/payment/wechat_pay_pm5ktx.png' alt='微信付款' class='h-96'>
     </div>
   </main>
 </template>
